1a67fb2de38d802e9d92932d4a07a4c33affa6c3
Bondo2-1 <k.medo417@gmail.com>
2021-11-13 14:11:59 +0200

    added files

---
+++ src/lol1/Lol1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lol1;
+
+/**
+ *
+ * @author mohamed
+ */
+public class Lol1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("lulu is the most broken support OP");
+    }
+    
+}
